86a11bb44863c92e31f89281b67f149334fa7c4a
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-16 00:03:59 -1000

    done

--- multiples.js
+++ multiples.js
@@ -7,6 +7,13 @@ exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
   // do your work here
+  for (var i = n - 1; i >= 0; i--) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
 
   return sum;
 };
+
+
